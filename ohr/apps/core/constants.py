e1fab495aa61e8dcd33deecf5aa59d510a3ec52c
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-03-21 11:19:11 +0300

    Add board number constants

--- ohr/apps/core/constants.py
+++ ohr/apps/core/constants.py
@@ -1,4 +1,8 @@
 """Constants module."""
+# strings
 FIRST_NAME = "First Name"
 LAST_NAME = "Last Name"
 EMAIL_ADDRESS = "Email Address"
+BOARD_NUMBER = "Board Number"
+# Form fields
+BOARD_NUM_FIELD = "board_num"
