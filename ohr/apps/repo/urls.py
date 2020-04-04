9924c28d9dabb1a44847ded7b3fa1a2485b02b95
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-04-04 14:23:11 +0300

    Add DocumentCreateView url

--- ohr/apps/repo/urls.py
+++ ohr/apps/repo/urls.py
@@ -1,12 +1,20 @@
 """repo app URL Configuration."""
 from django.urls import path
 
-from ohr.apps.repo.views import CategoryCRUD, DocumentCRUD, DocumentListView
+from ohr.apps.repo.views import (
+    CategoryCRUD,
+    DocumentCreateView,
+    DocumentCRUD,
+    DocumentListView,
+)
 
 # pylint: disable=invalid-name
 
 app_name = "repo"
 
-views = [path("documents", DocumentListView.as_view(), name="search")]
+views = [
+    path("documents", DocumentListView.as_view(), name="search"),
+    path("create", DocumentCreateView.as_view(), name="create"),
+]
 
 urlpatterns = views + DocumentCRUD().url_patterns() + CategoryCRUD().url_patterns()
