56462202ca03d84d7247531cf8f723fb27b87d95
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-04-04 14:22:36 +0300

    Add DocumentCreateView

--- ohr/apps/repo/views.py
+++ ohr/apps/repo/views.py
@@ -1,7 +1,42 @@
 """Views module for Vega Admin repo app."""
-from vega_admin.views import VegaCRUDView, VegaListView
+from vega_admin.utils import get_modelform
+from vega_admin.views import VegaCreateView, VegaCRUDView, VegaListView
 
 from ohr.apps.repo.models import Category, Document
+
+
+class DocumentCreateView(VegaCreateView):  # pylint: disable=too-many-ancestors
+    """List view for documents."""
+
+    model = Document
+    form_class = get_modelform(Document)
+    cancel_url = "/"
+
+    def get_initial(self):
+        """Get initial values for form."""
+        initial = super().get_initial()
+        if not initial.get("user"):
+            initial["user"] = self.request.user
+
+        return initial
+
+    def get_form_kwargs(self):
+        """Add kwargs to the form."""
+        kwargs = super().get_form_kwargs()
+        kwargs["request"] = self.request
+        if kwargs.get("vega_extra_kwargs"):
+            kwargs["vega_extra_kwargs"]["cancel_url"] = self.cancel_url
+        else:
+            kwargs["vega_extra_kwargs"] = {"cancel_url": self.cancel_url}
+        return kwargs
+
+
+class DocumentListView(VegaListView):  # pylint: disable=too-many-ancestors
+    """List view for documents."""
+
+    model = Document
+    search_fields = ["title", "description"]
+    template_name = "documents/list.html"
 
 
 class DocumentCRUD(VegaCRUDView):
@@ -20,11 +55,3 @@ class CategoryCRUD(VegaCRUDView):
     # protected_actions = None
     permissions_actions = None
     list_fields = ["id", "name"]
-
-
-class DocumentListView(VegaListView):  # pylint: disable=too-many-ancestors
-    """List view for documents."""
-
-    model = Document
-    search_fields = ["title", "description"]
-    template_name = "documents/list.html"
